04784b676bddf5e301a25bddf2ade6f191945808
vaporstack <andrew@vaporstack.org>
2018-03-21 18:11:28 -0400

    pencil stub

---
+++ src/wcm_tablet_pencil.h
@@ -0,0 +1,15 @@
+//
+//  wcm_tablet_pencil.h
+//  wcm
+//
+//  Created by vs on 3/21/18.
+//  Copyright © 2018 ruminant. All rights reserved.
+//
+
+#ifndef wcm_tablet_pencil_h
+#define wcm_tablet_pencil_h
+
+void wcm_tablet_pencil_init(void);
+void wcm_tablet_pencil_deinit(void);
+
+#endif /* wcm_tablet_pencil_h */
---
+++ src/wcm_tablet_pencil.m
@@ -0,0 +1,20 @@
+//
+//  wcm_tablet_pencil.c
+//  wcm
+//
+//  Created by vs on 3/21/18.
+//  Copyright © 2018 ruminant. All rights reserved.
+//
+
+#include "wcm_tablet_pencil.h"
+
+
+void wcm_tablet_pencil_init(void)
+{
+	
+}
+
+void wcm_tablet_pencil_deinit(void)
+{
+	
+}
